8343e126a4991a3ded72a48db1368204a3ef58bc
song2chan <haechandel@naver.com>
2021-05-31 18:34:09 +0900

    box animation add

--- js/todo.js
+++ js/todo.js
@@ -4,6 +4,7 @@ toDoInputBtn = document.querySelector(".to-do__send-btn"),
 toDoListBox = document.querySelector(".to-do__list");
 
 var toDoArray = [];
+var animating = false;
 
 function writeToDo(toDoText) {
   const oneListElement = document.createElement("li");
@@ -12,10 +13,10 @@ function writeToDo(toDoText) {
   const delBtnInnerIcon = `<i class="far fa-check-square"></i>`;
   
   delBtn.innerHTML = delBtnInnerIcon;
-  // delBtn.addEventListener("click", function() {
-  //   completeToDo(delBtn);
-  // })
-  delBtn.addEventListener("click", boxReSizing);
+  delBtn.addEventListener("click", function() {
+    deleteToDo(delBtn);
+  })
+
   toDoSpan.innerText = toDoText;
   oneListElement.appendChild(toDoSpan);
   oneListElement.appendChild(delBtn);
@@ -29,31 +30,58 @@ function writeToDo(toDoText) {
   toDoArray.push(newToDoObj);
 }
 
-// function completeToDo(targetBtn) {
+function deleteToDo(targetBtn) {
+  if(animating == false) {
+    animating = true;
+    setTimeout(() => {
+      animating = false;
+    }, 1000);
 
-// }
+    toDoArray.pop();
 
-function handleSubmit(event) {
+    deleteToDoAnim(targetBtn.parentNode);
+  
+    setTimeout(boxReSizing ,1000);
+    setTimeout(() => {
+      targetBtn.parentNode.remove();
+    }, 1000);
+  }
+}
+
+function handleSubmit() {
   // prevent page reload
   event.preventDefault();
 
   // input value로 writeToDo 함수 실행, 그리고 input value 초기화
   const curValue = toDoInput.value;
   if(curValue !== "") {
-    writeToDo(curValue);
+    setTimeout(writeToDo(curValue) ,3000);
+    setTimeout(boxReSizing ,1000);
     toDoInput.value ="";
   }
-
-  boxReSizing(toDoArray);
 }
 
 function boxReSizing() {
-  toDoListBox.style.height = toDoArray.length * 100 +"px";
-  console.log(toDoListBox.height());
+  const sizing =  + (toDoArray.length - 1 > 0 ? toDoArray.length - 1 : 0) * 32 +"px";
+
+  toDoListBox.animate([
+    { height: sizing }
+  ], 
+  {
+    duration: 1000,
+    easing: "ease-in-out",
+    fill: "forwards"
+  });
+}
+
+function deleteToDoAnim(target) {
+  target.style.backgroundColor =  "white";
+  target.style.opacity = "0";
 }
 
 function init() {
   toDoInputBtn.addEventListener("click", handleSubmit);
+  console.log(listBoxHeight);
 }
 
 init();
