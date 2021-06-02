59454b59879d87b1216e78f12624d89638bcc4c7
song2chan <haechandel@naver.com>
2021-06-02 21:05:43 +0900

    todo LS add

--- js/todo.js
+++ js/todo.js
@@ -3,15 +3,24 @@ toDoInput = document.querySelector(".to-do__writer input"),
 toDoInputBtn = document.querySelector(".to-do__send-btn"),
 toDoListBox = document.querySelector(".to-do__list");
 
+const TODOS_LS = "toDoData";
+
 var toDoArray = [];
 var animating = false;
 
+function saveToDos(ary) {
+  localStorage.setItem(TODOS_LS, JSON.stringify(ary));
+}
+
+// list에 들어갈 요소들을 만들어서 li로 넣어줌
 function writeToDo(toDoText) {
   const oneListElement = document.createElement("li");
   const toDoSpan = document.createElement("span");
   const delBtn = document.createElement("button");
   const delBtnInnerIcon = `<i class="far fa-check-square"></i>`;
-  
+  const elementId = toDoArray.length;
+
+  delBtn.className = elementId;
   delBtn.innerHTML = delBtnInnerIcon;
   delBtn.addEventListener("click", function() {
     deleteToDo(delBtn);
@@ -20,31 +29,58 @@ function writeToDo(toDoText) {
   toDoSpan.innerText = toDoText;
   oneListElement.appendChild(toDoSpan);
   oneListElement.appendChild(delBtn);
-  
-  toDoContents.appendChild(oneListElement);
 
   const newToDoObj = {
-    text: toDoText
+    text: toDoText,
+    id: elementId
   };
 
   toDoArray.push(newToDoObj);
+  saveToDos(toDoArray);
+  
+  setTimeout(() => {
+    toDoContents.appendChild(oneListElement);
+  }, 1500);
 }
 
 function deleteToDo(targetBtn) {
+  const animatingTime = 1000;
+  // animate 중에는 작동하지 않도록 함. (오류방지)
   if(animating == false) {
     animating = true;
     setTimeout(() => {
       animating = false;
-    }, 1000);
+    }, animatingTime);
 
-    toDoArray.pop();
+    const targetIdx = targetBtn.className * 1;
+    var newToDoArray = [];
 
-    deleteToDoAnim(targetBtn.parentNode);
+    targetBtn.parentNode.style.backgroundColor =  "white";
+    targetBtn.parentNode.style.opacity = "0";
   
-    setTimeout(boxReSizing ,1000);
+    setTimeout(boxReSizing ,animatingTime);
     setTimeout(() => {
       targetBtn.parentNode.remove();
-    }, 1000);
+    }, animatingTime);
+
+    // targetBtn의 className으로 toDoArray에서 target 객체를 찾은 뒤 삭제
+    // target이 아닌 객체 중 target의 id값보다 큰 객체들은 id를 -1씩 해주는데 그 전에 해당하는 className의
+    // 버튼들도 -1씩 해줌. 이러면 지운 공백이 메워지니까, 그런 뒤 해당 객체들을 새로운 배열에 저장
+    // 새로운 배열은 foreach문이 끝난 후 LS에 setItem 및 toDoArray에 덮어씌워줌.
+    toDoArray.forEach(function(target) {
+      if (toDoArray[targetIdx] !== target) {
+        if(targetIdx < target.id) {
+          document.getElementsByClassName(target.id).className = target.id -1;
+          target.id -= 1; 
+        }
+        newToDoArray.push(target);  
+      } else {
+        targetBtn.parentNode.remove(); 
+      }   
+    })
+
+    saveToDos(newToDoArray);
+    toDoArray = newToDoArray;
   }
 }
 
@@ -55,14 +91,17 @@ function handleSubmit() {
   // input value로 writeToDo 함수 실행, 그리고 input value 초기화
   const curValue = toDoInput.value;
   if(curValue !== "") {
-    setTimeout(writeToDo(curValue) ,3000);
+    writeToDo(curValue);
+  
     setTimeout(boxReSizing ,1000);
     toDoInput.value ="";
   }
 }
 
 function boxReSizing() {
-  const sizing =  + (toDoArray.length - 1 > 0 ? toDoArray.length - 1 : 0) * 32 +"px";
+  const baseHeight = getComputedStyle(toDoListBox).getPropertyValue("--baseHeight");
+
+  const sizing =  parseInt(baseHeight, 10) + (toDoArray.length - 1 > 0 ? toDoArray.length - 1 : 0) * 32 + "px";
 
   toDoListBox.animate([
     { height: sizing }
@@ -74,14 +113,21 @@ function boxReSizing() {
   });
 }
 
-function deleteToDoAnim(target) {
-  target.style.backgroundColor =  "white";
-  target.style.opacity = "0";
-}
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+   if (loadedToDos !== null) {
+     const parsedToDos = JSON.parse(loadedToDos);
+     parsedToDos.forEach(function(toDo) {
+       writeToDo(toDo.text);
+     })
+     boxReSizing();
+   }
+ }
 
 function init() {
   toDoInputBtn.addEventListener("click", handleSubmit);
-  console.log(listBoxHeight);
+
+  loadToDos();
 }
 
 init();
